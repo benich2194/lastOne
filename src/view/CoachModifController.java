4120b93d83c0f7d4941e851934fad1702542f0cc
benich2194 <benich2194@gmail.com>
2018-07-28 16:14:19 +0300

    added logout function

--- src/view/CoachModifController.java
+++ src/view/CoachModifController.java
@@ -23,6 +23,9 @@ public class CoachModifController {
     private Button addCoach;
     
     @FXML
+    private Button logout;
+    
+    @FXML
     void goBack(ActionEvent event) throws IOException {
     	Stage stage=(Stage)coachModif.getScene().getWindow();
 		stage.close();
@@ -44,5 +47,16 @@ public class CoachModifController {
     	primaryStage.setScene(scene);
     	primaryStage.show();
     }
-
+    
+    @FXML
+    void backToLogin(ActionEvent event) throws IOException {
+    	Stage stage=(Stage)coachModif.getScene().getWindow();
+		stage.close();
+    	FXMLLoader load=new FXMLLoader(getClass().getResource("/view/login.fxml"));
+    	Stage primaryStage=new Stage();
+    	Parent root=load.load();
+    	Scene scene=new Scene(root);
+    	primaryStage.setScene(scene);
+    	primaryStage.show();
+    }
 }
