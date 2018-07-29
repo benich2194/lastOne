472bbf43fa824a664a824aa50bddaed55ba08eda
benich2194 <benich2194@gmail.com>
2018-07-29 20:09:50 +0300

    add stadium

--- src/view/addStadiumController.java
+++ src/view/addStadiumController.java
@@ -1,24 +1,62 @@
 package view;
 
-import java.io.IOException;
+import java.awt.List;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import utils.E_Cities;
 
-public class addStadiumController {
+public class addStadiumController extends Main {
 
-    @FXML
-    private AnchorPane addStadium;
+	  @FXML
+	    private AnchorPane addStadium;
 
-    @FXML
-    private Button back;
+	    @FXML
+	    private Button back;
 
+	    @FXML
+	    private TextField id;
+
+	    @FXML
+	    private TextField name;
+	    
+	    @FXML
+	    private ChoiceBox<String> cityList;
+
+	    @FXML
+	    private TextField phone;
+
+	    @FXML
+	    private TextField street;
+
+	    @FXML
+	    private TextField capacity;
+
+	    @FXML
+	    private TextField houseNumber;
+
+	    @FXML
+	    private Button submit;
+	    
+
+	    
+	   
     @FXML
     void goBack(ActionEvent event) throws IOException {
     	Stage stage=(Stage)addStadium.getScene().getWindow();
@@ -29,6 +67,10 @@ public class addStadiumController {
     	Scene scene=new Scene(root);
     	primaryStage.setScene(scene);
     	primaryStage.show();
+    }
+    @FXML
+    void addStadium(ActionEvent event) {
+
     }
 
 }
