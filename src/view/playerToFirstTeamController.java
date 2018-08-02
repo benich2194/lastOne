85e8540b980743613f967cf1f5a85f70e6eb57e0
benich2194 <benich2194@gmail.com>
2018-08-02 13:08:56 +0300

    added add player to first team players

---
+++ src/view/playerToFirstTeamController.java
@@ -0,0 +1,67 @@
+package view;
+
+import Controller.SysData;
+import Model.Player;
+import Model.Team;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+
+public class playerToFirstTeamController {
+
+    @FXML
+    private AnchorPane playerToFirstTeam;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private ListView<Player> playerList;
+
+    @FXML
+    private ListView<Team> teamList;
+
+    @FXML
+    private Button connectThem;
+
+    @FXML
+    void addPlayerToFirstTeam(ActionEvent event) {
+    	Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Add Player To First Team Players");
+		alert.setHeaderText("");
+    	if(teamList.getSelectionModel().getSelectedItem()!=null&&playerList.getSelectionModel().getSelectedItem()!=null) {
+    		if(SysData.getInstance().addPlayerToTeamFirstPlayers(playerList.getSelectionModel().getSelectedItem().getId(),teamList.getSelectionModel().getSelectedItem().getId())){
+    			alert.setHeaderText("Added Player to First Team Players.");
+        		alert.setContentText("Player was added to First Team Players successfully.");
+        		alert.show();
+    		}
+    		else {
+    			alert.setHeaderText("failed to add Player to First Team Players.");
+        		alert.setContentText("unable to add Player to First Team Players, select a player and a team please.");
+        		alert.show();
+    		}
+    	}
+    	else {
+    		alert.setHeaderText("failed to add Player to First Team Players.");
+    		alert.setContentText("unable to add Player to First Team Players.");
+    		alert.show();
+    	}
+    }
+    public void initialize() {
+    	if(SysData.getInstance().getTeams().values().size()>0) {
+    		teamList.getItems().addAll(SysData.getInstance().getTeams().values());
+    	}
+    	if(SysData.getInstance().getPlayers().values().size()>0) {
+    		playerList.getItems().addAll(SysData.getInstance().getPlayers().values());
+    	}
+    }
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+}
