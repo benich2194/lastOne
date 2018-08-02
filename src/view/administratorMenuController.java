a4223af01b957928a8e0be76370d3171a5663f0b
gilitc <gilit.cherf@gmail.com>
2018-08-02 18:09:38 +0300

    admin button fixed

--- src/view/administratorMenuController.java
+++ src/view/administratorMenuController.java
@@ -1,7 +1,5 @@
 package view;
 
-import java.awt.event.ActionEvent;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
@@ -14,9 +12,5 @@ public class administratorMenuController {
     @FXML
     private Button pendingUsers;
 
-    @FXML
-    void goToPendingUsers(ActionEvent event) {
-
-    }
 
 }
