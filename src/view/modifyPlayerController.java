c3d6cd29e31582166585282b1e817eb9b9858913
gilitc <gilit.cherf@gmail.com>
2018-08-05 03:16:50 +0300

    In progress: ModifyPlayerController

--- src/view/modifyPlayerController.java
+++ src/view/modifyPlayerController.java
@@ -2,21 +2,33 @@ package view;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.List;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import utils.E_Cities;
 import utils.E_Levels;
 import utils.E_Position;
@@ -66,6 +78,7 @@ public class modifyPlayerController {
         @FXML
         private TableColumn<Player, Integer> playerID;
         
+        
     public void initialize() {
         // Defines how to fill data for each cell.
         // Get value from property of Player.
@@ -75,7 +88,7 @@ public class modifyPlayerController {
         playerPosition.setCellValueFactory(new PropertyValueFactory<>("position"));
         playerRLK.setCellValueFactory(new PropertyValueFactory<>("isRightLegKicker"));
         playerBday.setCellValueFactory(new PropertyValueFactory<>("birthdate"));
-        playerLvl.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        playerLvl.setCellValueFactory(new PropertyValueFactory<>("playerLvl"));
         playerVal.setCellValueFactory(new PropertyValueFactory<>("value"));
         playerAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
         playerSW.setCellValueFactory(new PropertyValueFactory<>("startWorkingDate"));
@@ -83,8 +96,100 @@ public class modifyPlayerController {
         // Display row data
         ObservableList<Player> list = FXCollections.observableArrayList(SysData.getInstance().getPlayers().values());
         playerTableView.setItems(list);
+        MakeEditable();
     }// End of modifyPlayer Constructor
         
+    private void MakeEditable() {
+    	
+    	// === On Cell edit commit (for FirstName column) ===
+    	playerFN.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+    	playerFN.setCellFactory(TextFieldTableCell.<Player> forTableColumn());
+    	playerFN.setOnEditCommit((CellEditEvent<Player, String> event) -> {
+            TablePosition<Player, String> pos = event.getTablePosition();
+ 
+            String newFirstName = event.getNewValue();
+ 
+            int row = pos.getRow();
+            Player pl = event.getTableView().getItems().get(row);
+ 
+            pl.setFirstName(newFirstName);
+        });
+    	
+    	// === On Cell edit commit (for LastName column) ===
+    	playerLN.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+    	playerLN.setCellFactory(TextFieldTableCell.<Player> forTableColumn());
+    	playerLN.setOnEditCommit((CellEditEvent<Player, String> event) -> {
+            TablePosition<Player, String> pos = event.getTablePosition();
+ 
+            String newLastName = event.getNewValue();
+ 
+            int row = pos.getRow();
+            Player pl = event.getTableView().getItems().get(row);
+ 
+            pl.setLastName(newLastName);
+        });
+ 
+        // ==== Player Position (COMBO BOX) ===
+ 
+        ObservableList<E_Position> positionList = FXCollections.observableArrayList(E_Position.values());
+ 
+        playerPosition.setCellValueFactory(new Callback<CellDataFeatures<Player, E_Position>, ObservableValue<E_Position>>() {
+ 
+            @Override
+            public ObservableValue<E_Position> call(CellDataFeatures<Player, E_Position> param) {
+                Player pl = param.getValue();
+
+                E_Position pos = pl.getPosition();
+                return new SimpleObjectProperty<E_Position>(pos);
+            }
+        });
+ 
+        playerPosition.setCellFactory(ComboBoxTableCell.forTableColumn(positionList));
+ 
+        playerPosition.setOnEditCommit((CellEditEvent<Player, E_Position> event) -> {
+            TablePosition<Player, E_Position> pos = event.getTablePosition();
+ 
+            E_Position newPos = event.getNewValue();
+ 
+            int row = pos.getRow();
+            Player pl = event.getTableView().getItems().get(row);
+ 
+            pl.setPosition(newPos);
+        });
+ 
+        // ==== RIGHT LEG KICKER? (CHECH BOX) ===
+        playerRLK.setCellValueFactory(new Callback<CellDataFeatures<Player, Boolean>, ObservableValue<Boolean>>() {
+ 
+            @Override
+            public ObservableValue<Boolean> call(CellDataFeatures<Player, Boolean> param) {
+                Player pl = param.getValue();
+ 
+                SimpleBooleanProperty booleanProp = new SimpleBooleanProperty(pl.isRightLegKicker());
+ 
+                // When "RLK?" column change.
+                booleanProp.addListener(new ChangeListener<Boolean>() {
+ 
+                    @Override
+                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
+                            Boolean newValue) {
+                        pl.setRightLegKicker(newValue);
+                    }
+                });
+                return booleanProp;
+            }
+        });
+ 
+        playerRLK.setCellFactory(new Callback<TableColumn<Player, Boolean>, //
+        TableCell<Player, Boolean>>() {
+            @Override
+            public TableCell<Player, Boolean> call(TableColumn<Player, Boolean> p) {
+                CheckBoxTableCell<Player, Boolean> cell = new CheckBoxTableCell<Player, Boolean>();
+                cell.setAlignment(Pos.CENTER);
+                return cell;
+            }
+        });
+    }//End of Method MAKEEDITABLE
+    
 	@FXML
     void goBack(ActionEvent event)  throws IOException{
 		//Go back to menu button Event
