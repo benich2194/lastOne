509c951160a10afd16aed18c3e3c6b30534602e9
benich2194 <benich2194@gmail.com>
2018-08-08 22:10:52 +0300

    added coach menu.not connected yet.

---
+++ src/view/CoachMenuSiderbarController.java
@@ -0,0 +1,56 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
+
+public class CoachMenuSiderbarController implements Initializable {
+
+	@FXML
+	private FlowPane menupane;
+
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		WindowManager.setContentPane(this.menupane);
+
+	}
+
+	public void loadUI(String ui) {
+		WindowManager.openWindow(ui);
+	}
+
+	@FXML
+	void goToAddPlayerToTeam(ActionEvent event) {
+		loadUI("coachAddPlayer");
+	}
+
+	@FXML
+	void goToChangeFirstTeam(ActionEvent event) {
+
+	}
+
+	@FXML
+	void goToModifyPlayerInTeam(ActionEvent event) {
+
+	}
+
+	@FXML
+	void goToQueries(ActionEvent event) {
+
+	}
+
+	@FXML
+	void goToViewPlayers(ActionEvent event) {
+
+	}
+
+	@FXML
+	void removePlayerFromTeam(ActionEvent event) {
+
+	}
+
+}
