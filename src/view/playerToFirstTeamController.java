a5122056200cf7913ebdabf9a39c0ff0ee0b35f9
gilitc <gilit.cherf@gmail.com>
2018-08-08 14:07:09 +0300

    Edited links and design in Conncetion Pages

--- src/view/playerToFirstTeamController.java
+++ src/view/playerToFirstTeamController.java
@@ -7,15 +7,11 @@ import Model.Player;
 import Model.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class playerToFirstTeamController {
 
@@ -67,14 +63,7 @@ public class playerToFirstTeamController {
     }
     @FXML
     void goBack(ActionEvent event) throws IOException {
-    	Stage stage=(Stage)connectThem.getScene().getWindow();
-		stage.close();
-    	FXMLLoader load=new FXMLLoader(getClass().getResource("/view/connectionMenu.fxml"));
-    	Stage primaryStage=new Stage();
-    	Parent root=load.load();
-    	Scene scene=new Scene(root);
-    	primaryStage.setScene(scene);
-    	primaryStage.show();
+    	WindowManager.goBack();
     }
 
 }
