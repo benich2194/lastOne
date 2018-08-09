4c35a25c54261d0678b24369d024b7ab861ff6bf
benich2194 <benich2194@gmail.com>
2018-08-09 11:39:38 +0300

    addded option first first team player in connection in main menu

--- src/view/playerToTeamController.java
+++ src/view/playerToTeamController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
@@ -27,28 +28,51 @@ public class playerToTeamController {
 
     @FXML
     private Button connectThem;
+    
+
+    @FXML
+    private ComboBox<Boolean> firstList;
 
     @FXML
     void addPlayerToTeam(ActionEvent event) {
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Add Player To Team");
 		alert.setHeaderText("");
-    	if(teamList.getSelectionModel().getSelectedItem()!=null&&playerList.getSelectionModel().getSelectedItem()!=null) {
-    		if(SysData.getInstance().addPlayerToTeam(playerList.getSelectionModel().getSelectedItem().getId(), teamList.getSelectionModel().getSelectedItem().getId())) {
-    			alert.setHeaderText("Added Player to team.");
-        		alert.setContentText("Player was added to team successfully.");
-        		alert.show();
+		Team t=teamList.getSelectionModel().getSelectedItem();
+    	if(playerList.getSelectionModel().getSelectedItem()!=null&&teamList.getSelectionModel().getSelectedItem()!=null) {
+    		if(firstList.getSelectionModel().getSelectedItem()) {
+    			if(SysData.getInstance().addPlayerToTeamFirstPlayers(playerList.getSelectionModel().getSelectedItem().getId(),t.getId() )) {
+        			alert.setHeaderText("Added Player to first team players.");
+            		alert.setContentText("Player was added to first team players successfully.");
+            		alert.show();
+        		}
+        		else {
+        			if(SysData.getInstance().addPlayerToTeam(playerList.getSelectionModel().getSelectedItem().getId(),t.getId())) {
+            			alert.setHeaderText("Added Player to team.");
+                		alert.setContentText("Player was added to team successfully.");
+                		alert.show();
+            		}
+            		else {
+            			alert.setHeaderText("failed to add Player to team.");
+                		alert.setContentText("unable to add Player to team, select a player and a team please.");
+                		alert.show();
+            		}
+        		}
     		}
-    		else {
-    			alert.setHeaderText("failed to add Player to team.");
-        		alert.setContentText("unable to add Player to team, select a player and a team please.");
-        		alert.show();
-    		}
+    		
     	}
     	else {
     		alert.setHeaderText("failed to add Player to team.");
     		alert.setContentText("unable to add Player to team.");
     		alert.show();
+    	}
+    	playerList.getItems().removeAll(playerList.getItems());
+    	if(SysData.getInstance().getPlayers()!=null) {
+    		for(Player p:SysData.getInstance().getPlayers().values()) {
+    			if(p!=null&&p.getCurrentTeam()==null) {
+    				playerList.getItems().add(p);
+    			}
+    		}
     	}
     }
 
@@ -64,6 +88,8 @@ public class playerToTeamController {
     	if(SysData.getInstance().getPlayers().values().size()>0) {
     		playerList.getItems().addAll(SysData.getInstance().getPlayers().values());
     	}
+    	firstList.getItems().add(true);
+    	firstList.getItems().add(false);
     
     }
 
