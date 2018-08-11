0709197933710c765b28b99b5846ad6a6f7d9f46
benich2194 <benich2194@gmail.com>
2018-08-11 12:46:12 +0300

    finished add player.defined text only and numbers only text fields.added exceptions.

--- src/view/addPlayerController.java
+++ src/view/addPlayerController.java
@@ -3,6 +3,9 @@ package view;
 import java.io.IOException;
 
 import Controller.SysData;
+import Exceptions.InvalidInputException;
+import Exceptions.ListNotSelectedException;
+import Exceptions.MissingInputException;
 import Model.Address;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,7 +21,9 @@ import utils.E_Levels;
 import utils.E_Position;
 
 public class addPlayerController {
-
+	/**
+	 * fxml fields
+	 */
     @FXML
     private AnchorPane addPlayer;
 
@@ -69,52 +74,112 @@ public class addPlayerController {
 
 
     @FXML
-    void addPlayer(ActionEvent event) {
+    void addPlayer(ActionEvent event)  throws MissingInputException,ListNotSelectedException, InvalidInputException{
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Add Player");
 		alert.setHeaderText("");
-    	Integer id = Integer.parseInt(playerId.getText());
-    	String first=firstName.getText();
-    	String last=lastName.getText();
-    	java.sql.Date bday = java.sql.Date.valueOf(birthDate.getValue());
-    	java.sql.Date work=java.sql.Date.valueOf(startWorkingDate.getValue());
-    	String[] phones=new String[1];
-    	phones[0]=playerPhone.getText();
-    	String street=playerStreet.getText();
-    	Integer houseNum=Integer.parseInt(houseNumber.getText());
-    	Address ad=new Address(playerCity.getSelectionModel().getSelectedItem(),street,houseNum,phones);
-    	Long val=Long.parseLong(playerValue.getText());
-    	if(SysData.getInstance().getPlayers().containsKey(id)) {
-    		alert.setHeaderText("Unable to add player.");
-    		alert.setContentText("Player already exists.");
-    		alert.show();
-    	}
-    	else {
-    		SysData.getInstance().addPlayer(id,"0",first,last,bday,work,playerLevel.getSelectionModel().getSelectedItem(),val,rightLeg.getSelectionModel().getSelectedItem(),playerPosition.getSelectionModel().getSelectedItem(),ad);
+		try {
+			Long val=Long.parseLong(playerValue.getText());
+			Integer id = Integer.parseInt(playerId.getText());
+	    	String first=firstName.getText();
+	    	String last=lastName.getText();
+	    	String[] phones=new String[1];
+	    	phones[0]=playerPhone.getText();
+	    	String street=playerStreet.getText();
+	    	Integer houseNum=Integer.parseInt(houseNumber.getText());
+	    	if(playerValue.getText()==""||playerId.getText()==""||first==""||last==""||playerPhone.getText()==""||street==""||houseNumber.getText()=="") {
+	    		throw new MissingInputException();
+	    	}
+	    	if(birthDate.getValue()==null||startWorkingDate.getValue()==null) {
+	    		throw new MissingInputException();
+	    	}
+	    	java.sql.Date bday = java.sql.Date.valueOf(birthDate.getValue());
+	    	java.sql.Date work=java.sql.Date.valueOf(startWorkingDate.getValue());
+	    	if(playerCity.getSelectionModel().getSelectedItem()==null||playerLevel.getSelectionModel().getSelectedItem()==null||rightLeg.getSelectionModel().getSelectedItem()==null||playerPosition.getSelectionModel().getSelectedItem()==null) {
+	    		throw new ListNotSelectedException();
+	    	}
+	    	Address ad=new Address(playerCity.getSelectionModel().getSelectedItem(),street,houseNum,phones);
+	    	
 	    	if(SysData.getInstance().getPlayers().containsKey(id)) {
-	    		alert.setHeaderText("Added Player");
-	    		alert.setContentText("Player added successfully.");
+	    		alert.setHeaderText("Unable to add player.");
+	    		alert.setContentText("Player already exists.");
 	    		alert.show();
 	    	}
 	    	else {
-	    		alert.setHeaderText("Unable to add Player.");
-	    		alert.setContentText("Player wasn't added.");
-	    		alert.show();
+	    		SysData.getInstance().addPlayer(id,"0",first,last,bday,work,playerLevel.getSelectionModel().getSelectedItem(),val,rightLeg.getSelectionModel().getSelectedItem(),playerPosition.getSelectionModel().getSelectedItem(),ad);
+		    	if(SysData.getInstance().getPlayers().containsKey(id)) {
+		    		alert.setHeaderText("Added Player");
+		    		alert.setContentText("Player added successfully.");
+		    		alert.show();
+		    	}
+		    	else {
+		    		alert.setHeaderText("Unable to add Player.");
+		    		alert.setContentText("Player wasn't added.");
+		    		alert.show();
+		    	}
 	    	}
-    	}
+		}catch(ListNotSelectedException e) {
+			
+		}catch(MissingInputException e) {
+			
+		}catch(NumberFormatException e) {
+			throw new InvalidInputException();
+		}
+    	
     }
-    
+    /**
+     * goes back to previous screen
+     * @param event back button is pressed
+     */
     @FXML
-    void goBack(ActionEvent event) throws IOException {
+    void goBack(ActionEvent event){
     	WindowManager.goBack();
     }
-    
+    /**
+     * initializes lists
+     */
     public void initialize() {
     	playerPosition.getItems().addAll(E_Position.values());
     	rightLeg.getItems().add(true);
     	rightLeg.getItems().add(false);
     	playerCity.getItems().addAll(E_Cities.values());
     	playerLevel.getItems().addAll(E_Levels.values());
+    	 firstName.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\sa-zA-Z*")) {
+		        	firstName.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+		        }
+		    });
+    	 lastName.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\sa-zA-Z*")) {
+		        	lastName.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+		        }
+		    });
+    	 playerStreet.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\sa-zA-Z*")) {
+		        	playerStreet.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+		        }
+		    });
+    	 playerPhone.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\d*")) {
+		        	playerPhone.setText(newValue.replaceAll("[^\\d]", ""));
+		        }
+		    });
+    	 playerValue.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\d*")) {
+		        	playerValue.setText(newValue.replaceAll("[^\\d]", ""));
+		        }
+		    });
+    	 playerId.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\d*")) {
+		        	playerId.setText(newValue.replaceAll("[^\\d]", ""));
+		        }
+		    });
+    	 houseNumber.textProperty().addListener((observable, oldValue, newValue) -> {
+		        if (!newValue.matches("\\d*")) {
+		        	houseNumber.setText(newValue.replaceAll("[^\\d]", ""));
+		        }
+		    });
+    	 
     }
 
 }
