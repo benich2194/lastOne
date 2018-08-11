be4d93749508a6df680cd3ed54c7641d5d3d9103
benich2194 <benich2194@gmail.com>
2018-08-11 12:03:16 +0300

    finished disconnect coach from team exceptions.

--- src/view/removeCoachFromTeamController.java
+++ src/view/removeCoachFromTeamController.java
@@ -1,6 +1,8 @@
 package view;
 
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
+import Exceptions.MissingInputException;
 import Model.Coach;
 import Model.Team;
 import javafx.event.ActionEvent;
@@ -10,7 +12,9 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 
 public class removeCoachFromTeamController {
-
+	/**
+	 * fxml fields
+	 */
     @FXML
     private AnchorPane removeCoach;
 
@@ -22,22 +26,49 @@ public class removeCoachFromTeamController {
 
     @FXML
     private Button removeButton;
-
+    /**
+     * goes back to previous screen
+     * @param event back button is pressed
+     */
     @FXML
     void goBack(ActionEvent event) {
     	WindowManager.goBack();
     }
-
+    /**
+     * removes coach from its team
+     * @param event	remove button is pressed
+     * @throws MissingInputException 
+     */
     @FXML
-    void removeCoach(ActionEvent event) {
-    	Coach c=coachList.getSelectionModel().getSelectedItem();
-    	if(c!=null) {
-    		Team t=c.getCurrentTeam();
-    		c.getTeams().add(t);
-    		c.setCurrentTeam(null);
-    		t.setCoach(null);
+    void removeCoach(ActionEvent event) throws ListNotSelectedException {
+    	try {
+    		Coach c=coachList.getSelectionModel().getSelectedItem();
+        	if(c!=null) {
+        		Team t=c.getCurrentTeam();
+        		c.getTeams().add(t);
+        		c.setCurrentTeam(null);
+        		t.setCoach(null);
+        	}
+        	else {
+        		throw new ListNotSelectedException();
+        	}
+        	if(SysData.getInstance().getCoachs()!=null) {
+        		for(Coach co:SysData.getInstance().getCoachs().values()) {
+        			if(co!=null) {
+        				if(co.getCurrentTeam()!=null) {
+        					coachList.getItems().add(co);
+        				}
+        			}
+        		}
+        	}
+    	}catch(ListNotSelectedException e) {
+    		
     	}
+    	
     }
+    /**
+     * initializes coach with team list
+     */
     public void initialize() {
     	if(SysData.getInstance().getCoachs()!=null) {
     		for(Coach c:SysData.getInstance().getCoachs().values()) {
