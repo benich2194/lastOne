258c665c0988e5dc78d8fad277a9357ad8fcdb2d
benich2194 <benich2194@gmail.com>
2018-08-11 12:29:48 +0300

    finished remove team with exceptions+javadoc

--- src/view/removeTeamController.java
+++ src/view/removeTeamController.java
@@ -3,6 +3,7 @@ package view;
 import java.io.IOException;
 
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
 import Model.Player;
 import Model.Stadium;
 import Model.Team;
@@ -16,7 +17,9 @@ import javafx.scene.layout.AnchorPane;
 
 public class removeTeamController {
 
-
+	/**
+	 * fxml fields
+	 */
     @FXML
     private AnchorPane removeTeam;
 
@@ -28,45 +31,63 @@ public class removeTeamController {
 
     @FXML
     private Button removeButton;
-
+    /**
+     * goes to previous screen
+     * @param event back button is pressed
+     */
     @FXML
-    void goBack(ActionEvent event) throws IOException {
+    void goBack(ActionEvent event) {
     	WindowManager.goBack();
     }
     
     @FXML
-    void removeTeam(ActionEvent event) {
+    void removeTeam(ActionEvent event) throws ListNotSelectedException {
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Remove Team");
 		alert.setHeaderText("");
-    	Team t=teamList.getSelectionModel().getSelectedItem();
-    	Stadium toRemoveTeamFrom = t.getStadium();
-    	//Remove team from it's current Stadium
-    	if(toRemoveTeamFrom!=null)
-    		toRemoveTeamFrom.removeTeam(t);
-    	//remove team from its players
-    	for(Player p:SysData.getInstance().getPlayers().values()) {
-    		if(p!=null) {
-    			if(p.getCurrentTeam()!=null) {
-    				if(p.getCurrentTeam().equals(t)) {
-    					p.setCurrentTeam(null);
-    				}
-    			}
-    		}
-    	}
-    	SysData.getInstance().getTeams().remove(t.getId());
-    	if(SysData.getInstance().getTeams().containsKey(t.getId())) {
-    		alert.setHeaderText("Team wasn't removed");
-    		alert.setContentText("Team wasn't removed successfully.");
-    		alert.show();
-    	}
-    	else {
-    		alert.setHeaderText("Team removed");
-    		alert.setContentText("Team was removed successfully.");
-    		alert.show();
+		try {
+			Team t=teamList.getSelectionModel().getSelectedItem();
+	    	if(t==null) {
+	    		throw new ListNotSelectedException();
+	    	}
+	    	Stadium toRemoveTeamFrom = t.getStadium();
+	    	//Remove team from it's current Stadium
+	    	if(toRemoveTeamFrom!=null)
+	    		toRemoveTeamFrom.removeTeam(t);
+	    	//remove team from its players
+	    	for(Player p:SysData.getInstance().getPlayers().values()) {
+	    		if(p!=null) {
+	    			if(p.getCurrentTeam()!=null) {
+	    				if(p.getCurrentTeam().equals(t)) {
+	    					p.setCurrentTeam(null);
+	    				}
+	    			}
+	    		}
+	    	}
+	    	SysData.getInstance().getTeams().remove(t.getId());
+	    	if(SysData.getInstance().getTeams().containsKey(t.getId())) {
+	    		alert.setHeaderText("Team wasn't removed");
+	    		alert.setContentText("Team wasn't removed successfully.");
+	    		alert.show();
+	    	}
+	    	else {
+	    		alert.setHeaderText("Team removed");
+	    		alert.setContentText("Team was removed successfully.");
+	    		alert.show();
+	    		if(SysData.getInstance().getTeams().size()>0) {//refreshes list
+	        		teamList.getItems().addAll(SysData.getInstance().getTeams().values());
+	        	}
+	    	}
+		}catch(ListNotSelectedException e) {
+			
+		}
+		if(SysData.getInstance().getTeams().size()>0) {//refreshes list
+    		teamList.getItems().addAll(SysData.getInstance().getTeams().values());
     	}
     }
-    
+    /**
+     * initializes list
+     */
     public void initialize() {
     	if(SysData.getInstance().getTeams().size()>0) {
     		teamList.getItems().addAll(SysData.getInstance().getTeams().values());
