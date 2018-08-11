3ea2ffa99b98d58758172ad77772637c7668b04b
benich2194 <benich2194@gmail.com>
2018-08-11 13:08:14 +0300

    finished remove player from team+exception+refresh

--- src/view/removePlayerFromTeamController.java
+++ src/view/removePlayerFromTeamController.java
@@ -1,6 +1,7 @@
 package view;
 
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
 import Model.Player;
 import Model.Team;
 import javafx.event.ActionEvent;
@@ -12,7 +13,9 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class removePlayerFromTeamController {
-
+	/**
+	 * fxml fields
+	 */
     @FXML
     private AnchorPane removePlayer;
 
@@ -24,30 +27,45 @@ public class removePlayerFromTeamController {
 
     @FXML
     private ListView<Player> playerList;
-
+    /**
+     * goes back to previous screen
+     * @param event back button is pressed
+     */
     @FXML
     void goBack(ActionEvent event) {
     	WindowManager.goBack();
     }
-
+    /**
+     * removes player from his team
+     * @param event remove player button is pressed
+     * @throws ListNotSelectedException
+     */
     @FXML
-    void removePlayerFromTeam(ActionEvent event) {
+    void removePlayerFromTeam(ActionEvent event) throws ListNotSelectedException {
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Remove Player From Team");
 		alert.setHeaderText("");
-    	Player p=playerList.getSelectionModel().getSelectedItem();
-    	Team t=p.getCurrentTeam();
-    	if(t.removePlayer(p)) {
-    		p.setCurrentTeam(null);
-    		alert.setHeaderText("Removed Player from team");
-    		alert.setContentText("Removed Player from team successfully.");
-    		alert.show();
-    	}
-    	else {
-    		alert.setHeaderText("Player removal from team failed.");
-    		alert.setContentText("Unable to remove player from team.");
-    		alert.show();
-    	}
+		try {
+			Player p=playerList.getSelectionModel().getSelectedItem();
+	    	Team t=p.getCurrentTeam();
+	    	if(t==null||p==null) {
+	    		throw new ListNotSelectedException();
+	    	}
+	    	if(t.removePlayer(p)) {
+	    		p.setCurrentTeam(null);
+	    		alert.setHeaderText("Removed Player from team");
+	    		alert.setContentText("Removed Player from team successfully.");
+	    		alert.show();
+	    	}
+	    	else {
+	    		alert.setHeaderText("Player removal from team failed.");
+	    		alert.setContentText("Unable to remove player from team.");
+	    		alert.show();
+	    	}
+		}catch(ListNotSelectedException e) {
+			
+		}
+    	playerList.getItems().removeAll(playerList.getItems());//refreshes list
     	for(Player pl:SysData.getInstance().getPlayers().values()) {
     		if(pl!=null&&pl.getCurrentTeam()!=null) {
     			playerList.getItems().add(pl);
@@ -55,6 +73,9 @@ public class removePlayerFromTeamController {
     	}
     	
     }
+    /**
+     * initializes player list
+     */
     public void initialize() {
     	for(Player p:SysData.getInstance().getPlayers().values()) {
     		if(p!=null&&p.getCurrentTeam()!=null) {
