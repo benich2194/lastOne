ed94d91dd311cc4374051c57d3a0f628c50f1536
benich2194 <benich2194@gmail.com>
2018-08-11 14:23:06 +0300

    finished with remove customer exception+javadoc+refresh

--- src/view/removeCustomerController.java
+++ src/view/removeCustomerController.java
@@ -3,6 +3,7 @@ package view;
 import java.io.IOException;
 
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
 import Model.Customer;
 import Model.Subscription;
 import javafx.event.ActionEvent;
@@ -14,7 +15,9 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class removeCustomerController {
-
+	/**
+	 * fxml fields
+	 */
 	@FXML
 	private AnchorPane removeCustomer;
 
@@ -26,33 +29,52 @@ public class removeCustomerController {
 
 	@FXML
 	private Button removeButton;
-
+	/**
+	 * goes back to previous screen
+	 * @param event back button is pressed
+	 */
 	@FXML
-	void goBack(ActionEvent event) throws IOException {
+	void goBack(ActionEvent event){
 		WindowManager.goBack();
 	}
-
+	/**
+	 * removes customer from data base and its subscriptions
+	 * @param event remove button is pressed
+	 * @throws ListNotSelectedException
+	 */
 	@FXML
-	void removeCustomer(ActionEvent event) {
+	void removeCustomer(ActionEvent event) throws ListNotSelectedException {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Remove Customer");
 		alert.setHeaderText("");
-		Customer c = cusList.getSelectionModel().getSelectedItem();
-		for (Subscription s : c.getSubscriptions()) {
-			SysData.getInstance().removeSubscription(s.getId());
+		try {
+			Customer c = cusList.getSelectionModel().getSelectedItem();
+			if(c==null)
+				throw new ListNotSelectedException();
+			for (Subscription s : c.getSubscriptions()) {
+				SysData.getInstance().removeSubscription(s.getId());
+			}
+			SysData.getInstance().getCustomers().remove(c.getId());
+			if (!SysData.getInstance().getCustomers().containsKey(c.getId())) {
+				alert.setHeaderText("Removed Customer");
+				alert.setContentText("Removed Customer successfully.");
+				alert.show();
+			} else {
+				alert.setHeaderText("Unable to remove Customer.");
+				alert.setContentText("Cannot remove Customer from database.");
+				alert.show();
+			}
+		}catch(ListNotSelectedException e) {
+			
 		}
-		SysData.getInstance().getCustomers().remove(c.getId());
-		if (!SysData.getInstance().getCustomers().containsKey(c.getId())) {
-			alert.setHeaderText("Removed Customer");
-			alert.setContentText("Removed Customer successfully.");
-			alert.show();
-		} else {
-			alert.setHeaderText("Unable to remove Customer.");
-			alert.setContentText("Cannot remove Customer from database.");
-			alert.show();
+		cusList.getItems().removeAll(cusList.getItems());
+		if (SysData.getInstance().getCustomers().size() > 0) {
+			cusList.getItems().addAll(SysData.getInstance().getCustomers().values());
 		}
 	}
-
+	/**
+	 * initializes customer list
+	 */
 	public void initialize() {
 		if (SysData.getInstance().getCustomers().size() > 0) {
 			cusList.getItems().addAll(SysData.getInstance().getCustomers().values());
