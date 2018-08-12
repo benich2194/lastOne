cba28846cf381d6dbc0d6147c59b02d5dd1cca74
benich2194 <benich2194@gmail.com>
2018-08-12 21:21:29 +0300

    finished coach menu functionality

--- src/view/coachAddPlayerToTeamController.java
+++ src/view/coachAddPlayerToTeamController.java
@@ -1,6 +1,7 @@
 package view;
 
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
 import Model.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,7 +13,9 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class coachAddPlayerToTeamController {
-
+	/**
+	 * fx fields
+	 */
     @FXML
     private AnchorPane playerToTeam;
 
@@ -24,39 +27,49 @@ public class coachAddPlayerToTeamController {
     
     @FXML
     private ComboBox<Boolean> firstList;
-
+    /**
+     * adds player to the team
+     * @param event add player button pressed
+     * @throws ListNotSelectedException
+     */
     @FXML
-    void addPlayerToTeam(ActionEvent event) {
+    void addPlayerToTeam(ActionEvent event) throws ListNotSelectedException {
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Add Player To Team");
 		alert.setHeaderText("");
-    	if(playerList.getSelectionModel().getSelectedItem()!=null) {
-    		if(firstList.getSelectionModel().getSelectedItem()) {
-    			if(SysData.getInstance().addPlayerToTeamFirstPlayers(playerList.getSelectionModel().getSelectedItem().getId(), Integer.parseInt(SysData.getInstance().getUserCoach()))) {
-        			alert.setHeaderText("Added Player to first team players.");
-            		alert.setContentText("Player was added to first team players successfully.");
-            		alert.show();
-        		}
-        		else {
-        			if(SysData.getInstance().addPlayerToTeam(playerList.getSelectionModel().getSelectedItem().getId(), Integer.parseInt(SysData.getInstance().getUserCoach()))) {
-            			alert.setHeaderText("Added Player to team.");
-                		alert.setContentText("Player was added to team successfully.");
-                		alert.show();
-            		}
-            		else {
-            			alert.setHeaderText("failed to add Player to team.");
-                		alert.setContentText("unable to add Player to team, select a player and a team please.");
-                		alert.show();
-            		}
-        		}
-    		}
-    		
-    	}
-    	else {
-    		alert.setHeaderText("failed to add Player to team.");
-    		alert.setContentText("unable to add Player to team.");
-    		alert.show();
-    	}
+		try {
+			if(playerList.getSelectionModel().getSelectedItem()==null||firstList.getSelectionModel().getSelectedItem()==null) {
+				throw new ListNotSelectedException();
+			}
+			if(playerList.getSelectionModel().getSelectedItem()!=null) {
+	    		if(firstList.getSelectionModel().getSelectedItem()) {
+	    			if(SysData.getInstance().addPlayerToTeamFirstPlayers(playerList.getSelectionModel().getSelectedItem().getId(), Integer.parseInt(SysData.getInstance().getUserCoach()))) {
+	        			alert.setHeaderText("Added Player to first team players.");
+	            		alert.setContentText("Player was added to first team players successfully.");
+	            		alert.show();
+	        		}
+	        		else {
+	        			if(SysData.getInstance().addPlayerToTeam(playerList.getSelectionModel().getSelectedItem().getId(), Integer.parseInt(SysData.getInstance().getUserCoach()))) {
+	            			alert.setHeaderText("Added Player to team.");
+	                		alert.setContentText("Player was added to team successfully.");
+	                		alert.show();
+	            		}
+	            		else {
+	            			alert.setHeaderText("failed to add Player to team.");
+	                		alert.setContentText("unable to add Player to team, select a player and a team please.");
+	                		alert.show();
+	            		}
+	        		}
+	    		}
+	    		
+	    	}
+	    	else {
+	    		throw new ListNotSelectedException();
+	    	}
+		}catch(ListNotSelectedException e) {
+			
+		}
+		//refreshes lists
     	playerList.getItems().removeAll(playerList.getItems());
     	if(SysData.getInstance().getPlayers()!=null) {
     		for(Player p:SysData.getInstance().getPlayers().values()) {
@@ -66,6 +79,9 @@ public class coachAddPlayerToTeamController {
     		}
     	}
     }
+    /**
+     * initializes lists
+     */
     public void initialize() {
     	if(SysData.getInstance().getPlayers()!=null) {
     		for(Player p:SysData.getInstance().getPlayers().values()) {
