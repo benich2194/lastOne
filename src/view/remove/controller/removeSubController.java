6699dddafcf9e0e00714e84d18bffe3bf77897f8
benich2194 <benich2194@gmail.com>
2018-08-12 16:22:05 +0300

    finished remove sub

--- src/view/remove/controller/removeSubController.java
+++ src/view/remove/controller/removeSubController.java
@@ -1,6 +1,7 @@
 package view.remove.controller;
 
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
 import Model.Coach;
 import Model.Customer;
 import Model.Player;
@@ -15,7 +16,9 @@ import javafx.scene.layout.AnchorPane;
 import view.WindowManager;
 
 public class removeSubController {
-
+	/**
+	 * fx fields
+	 */
     @FXML
     private AnchorPane removeSub;
 
@@ -27,26 +30,53 @@ public class removeSubController {
 
     @FXML
     private Button removeButton;
-
+    /**
+     * goes back to previous screen
+     * @param event back button is pressed
+     */
     @FXML
     void goBack(ActionEvent event) {
     	WindowManager.goBack();
     }
-
+    /**
+     * removes subscription from data base
+     * @param event remove button is pressed
+     * @throws ListNotSelectedException
+     */
     @FXML
-    void removeSubscription(ActionEvent event) {
+    void removeSubscription(ActionEvent event) throws ListNotSelectedException {
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Remove Subscription");
 		alert.setHeaderText("");
-    	if(SysData.getInstance().removeSubscription(subList.getSelectionModel().getSelectedItem().getId())) {
-    		alert.setHeaderText("Subscription was removed");
-    		alert.setContentText("Subscription was removed successfully.");
-    		alert.show();
-    	}
-    	else {
-    		alert.setHeaderText("Subscription wasn't removed");
-    		alert.setContentText("Subscription wasn't removed successfully.");
-    		alert.show();
+		try {
+			if(subList.getSelectionModel().getSelectedItem()==null) {
+				throw new ListNotSelectedException();
+			}
+			Subscription s=subList.getSelectionModel().getSelectedItem();
+			if(SysData.getInstance().removeSubscription(s.getId())) {
+	    		alert.setHeaderText("Subscription was removed");
+	    		alert.setContentText("Subscription was removed successfully.");
+	    		alert.show();
+	    	}
+	    	else {
+	    		alert.setHeaderText("Subscription wasn't removed");
+	    		alert.setContentText("Subscription wasn't removed successfully.");
+	    		alert.show();
+	    	}
+		}catch(ListNotSelectedException e) {
+			
+		}
+    	subList.getItems().removeAll(subList.getItems());
+    	if(SysData.getInstance().getCustomers().values()!=null) {//refreshes list
+    		for(Customer c:SysData.getInstance().getCustomers().values()) {
+    			if(c!=null&&c.getSubscriptions()!=null) {
+    				for(Subscription s:c.getSubscriptions()) {
+    					if(s!=null) {
+    						subList.getItems().add(s);
+    					}
+    				}
+    			}
+    		}
     	}
     	
     }
