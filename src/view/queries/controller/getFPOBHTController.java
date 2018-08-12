df9b0af52e6baea10ac489a2bbe86227b5d0c737
gilitc <gilit.cherf@gmail.com>
2018-08-12 20:05:02 +0300

    added Query 'first players on best home team'

---
+++ src/view/queries/controller/getFPOBHTController.java
@@ -0,0 +1,43 @@
+package view.queries.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import Controller.SysData;
+import Model.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import view.WindowManager;
+
+
+public class getFPOBHTController {
+
+    @FXML
+    private ListView<Player> playerMakerList;
+
+    @FXML
+    private AnchorPane addTeam;
+
+    @FXML
+    private Button back;
+
+
+    @FXML
+    void goBack(ActionEvent event) throws IOException {
+    	WindowManager.goBack();
+    }
+    
+    public void initialize() {
+    	ArrayList<Player> all = SysData.getInstance().getFirstPlayersOfBestHomeTeam();
+    	if(all.isEmpty()) {
+    		//playerMakerList.getItems().addAll(null);?????
+    	}
+    	else
+    		playerMakerList.getItems().addAll(all);
+    		
+    }
+
+    
+}
