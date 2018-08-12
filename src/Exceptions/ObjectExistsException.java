9025c7577a568e3d4f14bb7a1ec499717ba59d88
benich2194 <benich2194@gmail.com>
2018-08-12 15:50:21 +0300

    added another exception+finished add match exceptions

---
+++ src/Exceptions/ObjectExistsException.java
@@ -0,0 +1,19 @@
+package Exceptions;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class ObjectExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6814556722112607651L;
+	public ObjectExistsException(String m) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle(m+" "+"already exists");
+		alert.setHeaderText("");
+		alert.setHeaderText("Failed to continue,"+m+" already exists");
+		alert.show();
+	}
+}
--- src/view/add/controller/addMatchController.java
+++ src/view/add/controller/addMatchController.java
@@ -2,6 +2,9 @@ package view.add.controller;
 
 import java.io.IOException;
 import Controller.SysData;
+import Exceptions.ListNotSelectedException;
+import Exceptions.MissingInputException;
+import Exceptions.ObjectExistsException;
 import Model.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -45,47 +48,78 @@ public class addMatchController {
 
     @FXML
     private DatePicker matchDate;
-
+    /**
+     * adds match to database
+     * @param event add match button is pressed
+     * @throws MissingInputException
+     * @throws ListNotSelectedException
+     * @throws ObjectExistsException
+     */
     @FXML
-    void addMatch(ActionEvent event) {
+    void addMatch(ActionEvent event) throws MissingInputException, ListNotSelectedException, ObjectExistsException{
     	Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Add Match");
 		alert.setHeaderText("");
-		//ID is size of the map + 1, if exists, it will keep adding 1
-    	Integer id = SysData.getInstance().getMatchs().size()+1;
-    	while(SysData.getInstance().getMatchs().containsKey(id))
-    		id++;
-
-    	Integer extra=Integer.parseInt(Extra.getText());
-    	java.sql.Date date = null;
-    	if(matchDate!=null) {
-    		date=java.sql.Date.valueOf(matchDate.getValue());
-    	}
-    	if(SysData.getInstance().getMatchs().containsKey(id)) {
-    		alert.setHeaderText("Unable to add Match.");
-    		alert.setContentText("Match already exists.");
-    		alert.show();
-    	}
-    	else {
-    		SysData.getInstance().addMatch(id, date, extra, Home.getSelectionModel().getSelectedItem().getId(), Away.getSelectionModel().getSelectedItem().getId(), Integer.parseInt(homeS.getText()), Integer.parseInt(awayS.getText()));
-	    	if(SysData.getInstance().getMatchs().containsKey(id)) {
-	    		alert.setHeaderText("Added Match");
-	    		alert.setContentText("Match added successfully.");
-	    		alert.show();
+		try {
+			//ID is size of the map + 1, if exists, it will keep adding 1
+	    	Integer id = SysData.getInstance().getMatchs().size()+1;
+	    	while(SysData.getInstance().getMatchs().containsKey(id))
+	    		id++;
+	    	if(Extra.getText()=="") {
+	    		throw new MissingInputException("extra time");
+	    	}
+	    	Integer extra=Integer.parseInt(Extra.getText());
+	    	java.sql.Date date = null;
+	    	if(matchDate!=null) {
+	    		date=java.sql.Date.valueOf(matchDate.getValue());
 	    	}
 	    	else {
-	    		alert.setHeaderText("Unable to add Match.");
-	    		alert.setContentText("Match wasn't added.");
-	    		alert.show();
+	    		throw new MissingInputException("date of match");
 	    	}
-    	}
+	    	if(SysData.getInstance().getMatchs().containsKey(id)) {
+	    		throw new ObjectExistsException("match");
+	    	}
+	    	else {
+	    		if(Home.getSelectionModel().getSelectedItem()==null||Away.getSelectionModel().getSelectedItem()==null) {
+	    			throw new ListNotSelectedException();
+	    		}
+	    		if(homeS.getText()=="") {
+	    			throw new MissingInputException("home score");
+	    		}
+	    		if(awayS.getText()=="") {
+	    			throw new MissingInputException("away score");
+	    		}
+	    		SysData.getInstance().addMatch(id, date, extra, Home.getSelectionModel().getSelectedItem().getId(), Away.getSelectionModel().getSelectedItem().getId(), Integer.parseInt(homeS.getText()), Integer.parseInt(awayS.getText()));
+		    	if(SysData.getInstance().getMatchs().containsKey(id)) {
+		    		alert.setHeaderText("Added Match");
+		    		alert.setContentText("Match added successfully.");
+		    		alert.show();
+		    	}
+		    	else {
+		    		alert.setHeaderText("Unable to add Match.");
+		    		alert.setContentText("Match wasn't added.");
+		    		alert.show();
+		    	}
+	    	}
+		}catch(MissingInputException e) {
+			
+		}catch(ListNotSelectedException e) {
+			
+		}catch(ObjectExistsException e) {
+			
+		}
     }
-
+   /**
+    * goes back to previous screen
+    * @param event back button is pressed
+    */
     @FXML
-    void goBack(ActionEvent event) throws IOException {
+    void goBack(ActionEvent event) {
       	WindowManager.goBack();
     }
-    
+    /**
+     * initializes list and automatic id, defines text fields to be numbers only or letters only
+     */
     public void initialize() {
     	if(SysData.getInstance().getTeams().values().size()>0) {
     		Home.getItems().addAll(SysData.getInstance().getTeams().values());
@@ -93,13 +127,26 @@ public class addMatchController {
     	if(SysData.getInstance().getTeams().values().size()>0) {
     		Away.getItems().addAll(SysData.getInstance().getTeams().values());
     	}
-    
     	matchId.setEditable(false);
     	matchId.setDisable(true);
-    	
     	Integer idCurrent = SysData.getInstance().getMatchs().size()+1;
     	while(SysData.getInstance().getMatchs().containsKey(idCurrent))
     		idCurrent++;
-    	matchId.setText(idCurrent.toString());	
+    	matchId.setText(idCurrent.toString());
+    	Extra.textProperty().addListener((observable, oldValue, newValue) -> {
+	        if (!newValue.matches("\\d*")) {
+	        	Extra.setText(newValue.replaceAll("[^\\d]", ""));
+	        }
+	    });
+    	homeS.textProperty().addListener((observable, oldValue, newValue) -> {
+	        if (!newValue.matches("\\d*")) {
+	        	homeS.setText(newValue.replaceAll("[^\\d]", ""));
+	        }
+	    });
+    	awayS.textProperty().addListener((observable, oldValue, newValue) -> {
+	        if (!newValue.matches("\\d*")) {
+	        	awayS.setText(newValue.replaceAll("[^\\d]", ""));
+	        }
+	    });
     }
 }
