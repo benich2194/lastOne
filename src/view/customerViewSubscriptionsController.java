2f972aa325eb0f8136260eef5c0cbd7fe4fe4119
gilitc <gilit.cherf@gmail.com>
2018-08-13 14:53:43 +0300

    Customer can now see his own subscriptions. Edited page: customerViewSubscriptions

--- src/view/customerViewSubscriptionsController.java
+++ src/view/customerViewSubscriptionsController.java
@@ -1,5 +1,36 @@
 package view;
 
+import Controller.SysData;
+import Model.Subscription;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
 public class customerViewSubscriptionsController {
 
+	  @FXML
+	    private AnchorPane modifyTeam;
+
+	    @FXML
+	    private ListView<Subscription> listSubs;
+
+	    @FXML
+	    private Button back;
+
+	    public void initialize() {
+	    	String customerID = SysData.getInstance().getUserCustomer();
+	        ObservableList<Subscription> subs = FXCollections.observableArrayList(SysData.getInstance().getCustomers().get(customerID).getSubscriptions());
+	        listSubs.setItems(subs);
+	    }
+	    
+	    @FXML
+	    void goBack(ActionEvent event) {
+	    	WindowManager.goBack();
+	    }
 }
+
+
