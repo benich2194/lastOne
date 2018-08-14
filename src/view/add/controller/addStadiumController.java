c98d324b60d1e4a3c8e05814d2afc266c3b31209
benich2194 <benich2194@gmail.com>
2018-08-14 10:43:33 +0300

    finished add stadium

--- src/view/add/controller/addStadiumController.java
+++ src/view/add/controller/addStadiumController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
@@ -16,7 +17,7 @@ import utils.E_Cities;
 import view.Main;
 import view.WindowManager;
 
-public class addStadiumController{
+public class addStadiumController {
 	/**
 	 * fx fields
 	 */
@@ -50,6 +51,12 @@ public class addStadiumController{
 	@FXML
 	private Button submit;
 
+	@FXML
+	private Label labelSuccess;
+
+	@FXML
+	private Button clearButton;
+
 	/**
 	 * goes back to previous screen
 	 * 
@@ -59,6 +66,7 @@ public class addStadiumController{
 	void goBack(ActionEvent event) {
 		WindowManager.goBack();
 	}
+
 	/**
 	 * initializes lists, defines textfields for numbers only or letters only
 	 */
@@ -72,39 +80,41 @@ public class addStadiumController{
 			idCurrent++;
 		id.setText(idCurrent.toString());
 		street.textProperty().addListener((observable, oldValue, newValue) -> {
-	        if (!newValue.matches("\\sa-zA-Z*")) {
-	        	street.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
-	        }
-	    });
+			if (!newValue.matches("\\sa-zA-Z*")) {
+				street.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+			}
+		});
 		name.textProperty().addListener((observable, oldValue, newValue) -> {
-	        if (!newValue.matches("\\sa-zA-Z*")) {
-	        	name.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
-	        }
-	    });
+			if (!newValue.matches("\\sa-zA-Z*")) {
+				name.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+			}
+		});
 		capacity.textProperty().addListener((observable, oldValue, newValue) -> {
-	        if (!newValue.matches("\\d*")) {
-	        	capacity.setText(newValue.replaceAll("[^\\d]", ""));
-	        }
-	    });
+			if (!newValue.matches("\\d*")) {
+				capacity.setText(newValue.replaceAll("[^\\d]", ""));
+			}
+		});
 		phone.textProperty().addListener((observable, oldValue, newValue) -> {
-	        if (!newValue.matches("\\d*")) {
-	        	phone.setText(newValue.replaceAll("[^\\d]", ""));
-	        }
-	    });
+			if (!newValue.matches("\\d*")) {
+				phone.setText(newValue.replaceAll("[^\\d]", ""));
+			}
+		});
 		houseNumber.textProperty().addListener((observable, oldValue, newValue) -> {
-	        if (!newValue.matches("\\d*")) {
-	        	houseNumber.setText(newValue.replaceAll("[^\\d]", ""));
-	        }
-	    });
+			if (!newValue.matches("\\d*")) {
+				houseNumber.setText(newValue.replaceAll("[^\\d]", ""));
+			}
+		});
 	}
+
 	/**
 	 * adds stadium
+	 * 
 	 * @param event add button is pressed
 	 * @throws MissingInputException
 	 * @throws ListNotSelectedException
 	 */
 	@FXML
-	void addStadium(ActionEvent event) throws MissingInputException,ListNotSelectedException,NumberFormatException {
+	void addStadium(ActionEvent event) throws MissingInputException, ListNotSelectedException, NumberFormatException {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Add Stadium");
 		alert.setHeaderText("");
@@ -114,28 +124,28 @@ public class addStadiumController{
 			while (SysData.getInstance().getStadiums().containsKey(id))
 				id++;
 			String NAME = name.getText();
-			if(NAME.isEmpty()) {
+			if (NAME.isEmpty()) {
 				throw new MissingInputException("name");
 			}
-			
-			if(houseNumber.getText().isEmpty()) {
+
+			if (houseNumber.getText().isEmpty()) {
 				throw new MissingInputException("house number");
 			}
 			Integer HOUSENUM = Integer.parseInt(houseNumber.getText());
-			if(capacity.getText().isEmpty()) {
+			if (capacity.getText().isEmpty()) {
 				throw new MissingInputException("capacity");
 			}
 			Integer CAPACITY = Integer.parseInt(capacity.getText());
 			String PHONE = phone.getText();
-			if(PHONE.isEmpty()) {
+			if (PHONE.isEmpty()) {
 				throw new MissingInputException("phone");
 			}
 			E_Cities c = cityList.getSelectionModel().getSelectedItem();
-			if(c==null) {
+			if (c == null) {
 				throw new ListNotSelectedException();
 			}
 			String STREET = street.getText();
-			if(STREET.isEmpty()) {
+			if (STREET.isEmpty()) {
 				throw new MissingInputException("street");
 			}
 			String[] phoneArr = new String[1];
@@ -147,9 +157,14 @@ public class addStadiumController{
 			} else {
 				SysData.getInstance().addStadium(id, NAME, CAPACITY, c, STREET, HOUSENUM, phoneArr);
 				if (SysData.getInstance().getStadiums().containsKey(id)) {
-					alert.setHeaderText("Added Stadium");
-					alert.setContentText("Stadium was added succesfully");
-					alert.show();
+					labelSuccess.setText("Stadium "+id+" was added succesfully!");
+					cityList.valueProperty().set(null);
+					this.id.setText("");
+					street.setText("");
+					capacity.setText("");
+					name.setText("");
+					phone.setText("");
+					houseNumber.setText("");
 				} else {
 					alert.setHeaderText("Unable to add stadium");
 					alert.setContentText("Stadium wasn't added to stadiums.");
@@ -157,14 +172,28 @@ public class addStadiumController{
 				}
 			}
 
-		}catch(NumberFormatException e) {
-			
-		}catch(ListNotSelectedException e) {
-			
-		}catch(MissingInputException e) {
-			
+		} catch (NumberFormatException e) {
+
+		} catch (ListNotSelectedException e) {
+
+		} catch (MissingInputException e) {
+
 		}
-		}
-		
+	}
+	/**
+	 * clears form
+	 * @param event clear button is pressed
+	 */
+	@FXML
+	void clearForm(ActionEvent event) {
+		cityList.valueProperty().set(null);
+		id.setText("");
+		street.setText("");
+		capacity.setText("");
+		name.setText("");
+		phone.setText("");
+		houseNumber.setText("");
+		labelSuccess.setText("");
+	}
 
 }
