5c02f0c16d83a32196457cc1a9d2889167aa2cc0
benich2194 <benich2194@gmail.com>
2018-08-15 13:09:30 +0300

    finished add coach to team.please check why coach 8 to team 8 fails

--- src/view/coachToTeamController.java
+++ src/view/coachToTeamController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
@@ -30,15 +31,26 @@ public class coachToTeamController {
 
     @FXML
     private Button connectThem;
+    
+    @FXML
+    private Label labelSuccess;
     /**
      * initializes lists
      */
     public void initialize() {
     	if(SysData.getInstance().getTeams().values().size()>0) {
-    		teamList.getItems().addAll(SysData.getInstance().getTeams().values());
+    		for(Team t:SysData.getInstance().getTeams().values()) {
+    			if(t!=null&&t.getCoach()==null) {
+    				teamList.getItems().add(t);
+    			}
+    		}
     	}
     	if(SysData.getInstance().getCoachs().values().size()>0) {
-    		coachList.getItems().addAll(SysData.getInstance().getCoachs().values());
+    		for(Coach c:SysData.getInstance().getCoachs().values()) {
+    			if(c!=null&&c.getCurrentTeam()==null) {
+    				coachList.getItems().add(c);
+    			}
+    		}
     	}
     
     }
@@ -49,11 +61,11 @@ public class coachToTeamController {
 		alert.setTitle("Add Coach To Team");
 		alert.setHeaderText("");
 		try {
+			Coach c=coachList.getSelectionModel().getSelectedItem();
+			Team t=teamList.getSelectionModel().getSelectedItem();
 			if(teamList.getSelectionModel().getSelectedItem()!=null&&coachList.getSelectionModel().getSelectedItem()!=null) {
 	    		if(SysData.getInstance().addCoachToTeam(coachList.getSelectionModel().getSelectedItem().getId(), teamList.getSelectionModel().getSelectedItem().getId())) {
-	    			alert.setHeaderText("Added coach to team.");
-	        		alert.setContentText("Coach was added to team successfully.");
-	        		alert.show();
+	    			labelSuccess.setText("coach "+c.getId()+" was added to team "+t.getId());
 	    		}
 	    		else {
 	    			alert.setHeaderText("failed to add coach to team.");
@@ -71,10 +83,18 @@ public class coachToTeamController {
     	teamList.getItems().removeAll(teamList.getItems());
     	coachList.getItems().removeAll(coachList.getItems());
     	if(SysData.getInstance().getTeams().values().size()>0) {
-    		teamList.getItems().addAll(SysData.getInstance().getTeams().values());
+    		for(Team t:SysData.getInstance().getTeams().values()) {
+    			if(t!=null&&t.getCoach()==null) {
+    				teamList.getItems().add(t);
+    			}
+    		}
     	}
     	if(SysData.getInstance().getCoachs().values().size()>0) {
-    		coachList.getItems().addAll(SysData.getInstance().getCoachs().values());
+    		for(Coach c:SysData.getInstance().getCoachs().values()) {
+    			if(c!=null&&c.getCurrentTeam()==null) {
+    				coachList.getItems().add(c);
+    			}
+    		}
     	}
     }
 
