38ac931f8c6aa6c954d4bb4f97151aac35bfe269
gilitc <gilit.cherf@gmail.com>
2018-08-22 18:17:16 +0300

    Added logout button to admin menu

--- src/view/AdminMenuController.java
+++ src/view/AdminMenuController.java
@@ -1,11 +1,19 @@
 package view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import Controller.SysData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 import utils.NameToWindow;
 
 public class AdminMenuController implements Initializable{
@@ -13,6 +21,9 @@ public class AdminMenuController implements Initializable{
     @FXML
     private FlowPane menupane;
 
+    @FXML
+    private Button logout;
+    
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
@@ -82,5 +93,22 @@ public class AdminMenuController implements Initializable{
     	loadUI(NameToWindow.QUERIES);
     }
 
+    @FXML
+    void GoToLogin(ActionEvent event) throws IOException {
+		Stage stage = (Stage) menupane.getScene().getWindow();
+		
+		SysData.getInstance().setUserCoach(null);
+		SysData.getInstance().setUserCustomer(null);
+		SysData.getInstance().setUserRecep(null);
+		
+		stage.close();
+		FXMLLoader load = new FXMLLoader(getClass().getResource("/view/login.fxml"));
+		Stage primaryStage = new Stage();
+		Parent root = load.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+    
 }
 
