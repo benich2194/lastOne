8a24d786a7da87e7e84f87f2b78e0887ffd0fdde
gilitc <gilit.cherf@gmail.com>
2018-08-22 16:20:22 +0300

    Added view first team players to View Teams. Important: to be tested, open sysdata.ser file, delete all content and add new items.

--- src/view/view/controller/viewTeamsController.java
+++ src/view/view/controller/viewTeamsController.java
@@ -1,5 +1,8 @@
 package view.view.controller;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import Controller.SysData;
 import Model.Match;
 import Model.Player;
@@ -33,7 +36,9 @@ public class viewTeamsController {
     @FXML
     private ListView<Match> teamsMatches;
 
-	
+    @FXML
+    private ListView<Player> teamPlayersListFirst;
+    
     public void initialize() {
     	if(!SysData.getInstance().getTeams().isEmpty())
     	{
@@ -51,14 +56,27 @@ public class viewTeamsController {
     }
     
     private void showRowDetails(Team item) {
-        ObservableList<Player> teamplayers = FXCollections.observableArrayList(item.getPlayers().keySet());
+    	ArrayList<Player> allPlys = new ArrayList<Player>(item.getPlayers().keySet());
+        ObservableList<Player> teamplayers = FXCollections.observableArrayList(allPlys);
         ObservableList<Match> matchestm = FXCollections.observableArrayList(item.getMatches());
         if(!teamplayers.isEmpty())
         	teamPlayersList.setItems(teamplayers);
         
         if(!matchestm.isEmpty())
         	teamsMatches.setItems(matchestm);
+
+        //Display First Team Players
+        //Remove everyone who is not a first team player, from allPlys ArrayList
+        for(Map.Entry<Player, Boolean> entry: item.getPlayers().entrySet()) {
+        	if(!entry.getValue())
+        		allPlys.remove(entry.getKey());
+        }
+        
+        ObservableList<Player> firstTeamplayers = FXCollections.observableArrayList(allPlys);
+        if(!firstTeamplayers.isEmpty())
+        	teamPlayersList.setItems(firstTeamplayers);
     }
+    
     /**
      * goes back to previous screen
      * @param event back button is pressed
